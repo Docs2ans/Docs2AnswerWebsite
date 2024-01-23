ada839b55a488f577dad53b79f64597ea88d8fcb
suyashLade <suyashlade@gmail.com>
2024-01-23 20:00:09 +0530

    added google analytics id

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Banner from "@/components/home/banner";
 import WhatWeOffer from "@/components/home/whatweoffer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function Home() {
   return (
@@ -11,6 +12,8 @@ export default function Home() {
         //     "url(https://assets-global.website-files.com/63ce8a45d805d5c08514e388/63dde8386bf1b16f7959f42f_hero%20Grains.svg)",
         // }}
       >
+        <GoogleAnalytics gaId="G-KWXZMC8LW6" />
+
         <Banner />
         {/* <BookADemo /> */}
         <WhatWeOffer />
